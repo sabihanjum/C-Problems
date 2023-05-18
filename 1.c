d0f92c065d0aa6638a63b927202a2f06b5f30fba
Sabiha Anjum <112552772+sabihanjum@users.noreply.github.com>
2023-05-18 22:02:59 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+
+void main()
+{
+
+    
+   int Test = 50;
+   if(Test >= 10)
+   {
+    printf("%d", Test);
+   }
+   Test = Test + 100;
+   printf("%d", Test);
+}
