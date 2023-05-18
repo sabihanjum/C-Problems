9777544be337a9c2ed45bdf7212595bd2e4bdba6
Sabiha Anjum <112552772+sabihanjum@users.noreply.github.com>
2023-05-18 22:12:08 +0530

    Update and rename 1.c to helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+/*First program in C language*/
+#include<stdio.h> //Header fils
+#include<conio.h>
+
+void main()    //main function
+{
+    print{"Hello world!!"}; //output printing
+    
+}
+
+
+
+//OUTPUT:
+Hello world!!
